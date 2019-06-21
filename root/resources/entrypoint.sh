2b35e0b74cf2dbd1d24cb2dcc63d82955772af49
Gerben Tiele <g.tiele@gmail.com>
2019-06-21 17:11:27 +0200

    Gracefull shutdown of Smokeping and Apache in case of docker stop Smokeping init.d no longer needed

--- root/resources/entrypoint.sh
+++ root/resources/entrypoint.sh
@@ -5,5 +5,30 @@ bash /resources/dir.sh
 bash /resources/config.sh
 bash /resources/timezone.sh
 
-su bogus -c 'smokeping'
-/usr/sbin/apache2ctl -D FOREGROUND
+touch /var/log/smokeping.log
+chown bogus /var/log/smokeping.log
+su bogus -c 'smokeping --logfile=/var/log/smokeping.log'
+/usr/sbin/apache2ctl start
+
+cleanup() {
+    apache2ctl stop
+	su bogus -c 'service smokeping stop'
+
+	while :; do
+		if pgrep -x "apache2" >/dev/null && pgrep -x "smokeping" >/dev/null
+		then
+		    echo "Waiting for Apache & Smokeping to shutdown"
+		else
+		    exit 0
+		fi
+
+		sleep 1s
+	done
+}
+
+trap cleanup INT TERM
+
+while :; do
+    #never ending loop
+    sleep 1s
+done
